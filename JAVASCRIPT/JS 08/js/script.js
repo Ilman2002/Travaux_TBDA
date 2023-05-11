e82084d454cc30552576ef71117304304ccc67c2
Kagermanov <kagermanovilman@gmail.com>
2023-05-11 11:36:15 +0200

    JS 08 exercice 3

---
+++ JAVASCRIPT/JS 08/js/script.js
@@ -0,0 +1,7 @@
+var a = prompt("Entrez le premier nombre");
+var b = prompt("Entrer le deuxième nombre");
+var somme = parseInt(a) + parseInt(b);
+var moyenne=somme/2;
+var mult=a*b;
+document.write("La somme de "+a+" et "+b+" égal à "+ somme +"<br>");
+document.write("La moyenne de "+a+" et "+b+" égal à "+ moyenne +"<br>");
