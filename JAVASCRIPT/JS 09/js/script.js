dbad544a60824d72df75f76614b1fdf32ebe6bd9
Kagermanov <kagermanovilman@gmail.com>
2023-05-11 14:30:00 +0200

    JS 09 exercice 1

---
+++ JAVASCRIPT/JS 09/js/script.js
@@ -0,0 +1,6 @@
+var a = prompt("entrez un nombre");
+var b = prompt("entrer un multiplicateur");
+var cube = a*a*a
+var produit = a*b
+document.write("Le cube de "+a+" égal à "+ cube +"<br>");
+document.write("Le produit de "+a+" et "+b+" égal à "+ produit +"<br>");
