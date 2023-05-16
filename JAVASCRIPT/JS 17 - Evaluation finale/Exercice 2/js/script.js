09a9888a293c6871035430efd741f4adddaa0c44
Kagermanov <kagermanovilman@gmail.com>
2023-05-16 15:48:19 +0200

    Exercice 2

--- JAVASCRIPT/JS 17 - Evaluation finale/Exercice 2/js/script.js
+++ JAVASCRIPT/JS 17 - Evaluation finale/Exercice 2/js/script.js
@@ -1,7 +1,10 @@
-function TableMultiplication(n) {
-    for (let i = 1; i <= 10; i++) {
-      document.write(i + " x " + n + " = " + i * n + "<br>");
-    }
-  }
+function afficherTable() { 
+  var input = document.getElementById("nombre")
+  var n = parseInt(input.value);
+  var table = "";
 
-  TableMultiplication(97);  /*Préciser la table de multiplication à afficher*/
+  for (var i = 1; i <= 10; i++) {
+      table += n + " x " + i + " = " + (n * i) + "\n";
+    }
+    alert("Table de multiplication de " + n + " :\n\n" + table);
+  }
